9d4e657e7323e734812c44a6737ee5006d30f05f
SteshM <mbithestella0@gmail.com>
2023-05-18 08:36:07 +0300

    :tada: My  Student API

--- src/main/java/com/skills/springbootexample/Main.java
+++ src/main/java/com/skills/springbootexample/Main.java
@@ -1,9 +1,14 @@
 package com.skills.springbootexample;
 
+import com.skills.springbootexample.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +18,16 @@ public class Main {
 		SpringApplication.run(Main.class, args);
 	}
 	@GetMapping("/")
-	public String greet(){
-	     return "hello Stellaaa" ;
+	public List<Student> hello(){
+	     return  List.of(
+				 new Student(
+						 1L,
+		                 "Stesh",
+		                 "stesh@gmail.com ",
+						 LocalDate.of( 2001, Month.JANUARY, 15),
+						 22
+				 )
+		 );
 	}
 
 }
---
+++ src/main/java/com/skills/springbootexample/student/Student.java
@@ -0,0 +1,23 @@
+package com.skills.springbootexample.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    public Long id;
+    public String name;
+    public String email;
+    public LocalDate dob;
+    public Integer age;
+
+    public Student(Long id,
+                   String name,
+                   String email,
+                   LocalDate dob,
+                   Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+}
